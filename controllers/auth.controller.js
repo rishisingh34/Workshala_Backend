8017156d784d4f33280460d866e3eb27df528797
rishisingh34 <singh34rishi@gmail.com>
2023-11-20 16:42:38 +0530

    JOI Validation Schema Added _Email_Number_Name_Password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,50 +3,87 @@ const bcryptjs = require("bcryptjs");
 const { token } = require("../utils/token.util");
 const { sendmail , sendOtpMail} = require("../utils/mailer.util");
 const Otp = require("../models/otp.model");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const tokenExpiration = Date.now() + 24 * 60 * 60 * 1000;
+const authSchema = require('../utils/validationSchema.util')
 
 const authCtrl = {
   signUp: async (req, res) => {
     try {
       // storing responses recieved from client side
-      const { email, password, name, number } = req.body;
+      // const { email, password, name, number } = req.body;
+
+      const validationErrors = {};
+
+      try {
+        await authSchema.validateAsync(req.body);
+      } catch (validationErr) {
+        validationErr.details.forEach((error) => {
+          if (!validationErrors[error.path[0]]) {
+            validationErrors[error.path[0]] = [];
+          }
+          validationErrors[error.path[0]].push(error.message);
+        });
+      }
+
+      if (Object.keys(validationErrors).length > 0) {
+        return res.status(400).json({ errors: validationErrors });
+      }
+
+      const result = await authSchema.validateAsync(req.body);
+      const { email, password, name, number } = result;
 
       const user = await User.findOne({ email: email });
 
       const hashedPassword = await bcryptjs.hash(password, 8); // hashing Password using bcrypt
-      const token = crypto.randomBytes(20).toString("hex"); // generating verification token for email verfication in database
 
-      const verificationLink = `https://workshala.onrender.com/verifyEmailPage?token=${token}`;
-
-      if(user){
+      if (user) {
         if (user.isVerified) {
           res.status(409).json({ message: "User already exists" });
           return;
         } else {
-          user.updateOne({
-            email: email,
-            password: password,
-            name: name,
-            contact: number,
-            isVerified: false,
-            verificationToken: {
-              token: token,
-              expiration: tokenExpiration,
-            },
-          });
+          if (
+            !user.verificationToken.expiration ||
+            user.verificationToken.expiration < Date.now()
+          ) {
+            const newToken = crypto.randomBytes(20).toString("hex");
 
-          sendmail(email, verificationLink, "Email Verificaition Link"); // verification link sent to existing user again ( email !verified)
+            await User.findOneAndUpdate(
+              { email: email },
+              {
+                $set: {
+                  name: name,
+                  password: hashedPassword,
+                  contact: number,
+                  verificationToken: {
+                    token: newToken,
+                    expiration: tokenExpiration,
+                  },
+                },
+              }
+            );
 
-          res
-            .status(200)
-            .json({
-              message: "User Data Updated Successfully. Email Sent Again",
+            res.status(200).json({
+              message:
+                "User updated successfully. Verification Link Sent Again",
             });
-          return;
+            return;
+          } else {
+            res.status(200).json({
+              message:
+                "Verification Link Already Sent. Check Your Email. New Verification Link Will Be Sent After 24Hrs",
+            });
+            return;
+          }
         }
       }
+
+      // generating verification token for email verfication in database
+      const token = crypto.randomBytes(20).toString("hex");
+
+      // attaching verifcation token in Link
+      const verificationLink = `https://workshala.onrender.com/verifyEmailPage?token=${token}`;
 
       const newUser = new User({
         name: name,
@@ -88,7 +125,10 @@ const authCtrl = {
 
       await User.findOneAndUpdate(
         { "verificationToken.token": token },
-        { $set: { isVerified: true } }
+        { 
+          $set: { isVerified: true },
+          $unset : { verificationToken : 1 }
+        }
       );
 
       res.render("emailVerification"); // Rendering the Successful Verification Page
--- utils/validationSchema.util.js
+++ utils/validationSchema.util.js
@@ -1,2 +1,27 @@
-// lecture 7 mafia codes 
+const Joi = require("joi");
 
+const authSchema = Joi.object({
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  password: Joi.string()
+    .pattern(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[a-zA-Z]).{8,}$/
+    )
+    .message(
+      "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 digit, 1 special character, and be at least 8 characters long"
+    )
+    .required(),
+  name: Joi.string()
+    .regex(/^[a-zA-Z]{3,}$/)
+    .message("Name must be alphabetic and at least 3 characters long")
+    .required(),
+  number: Joi.string()
+    .pattern(/^[6-9]\d{9}$/)
+    .message("Please enter a valid 10-digit Indian mobile number")
+    .required(),
+});
+
+
+module.exports = authSchema;
